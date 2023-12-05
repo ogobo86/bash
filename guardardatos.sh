474e17271f79ad5410931ade3a973a7f29192fbf
Oscar Gonzalez <ogonbon@gmail.com>
2023-12-04 22:50:00 -0600

    update
    aprendiendo bash

---
+++ guardardatos.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+#Guarda el texto           en  nombrearchivo.txt
+echo 'Guardar informacion' >  guarda.txt
+
+#al ejecuatar ./guardardatos , se crear un archivo txt unicamente con echo, pero al ejecutar denuevamente el archivo SOBREESSCRIBE
+
+cat >> guardartextocorrido.txt
+
+#al ejecutar cat >> nombretexto.txt guarda de manera corrida la informacion recolectada en consola
---
+++ helloworld.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+#Comando ECHO sirve para mandar "imprimir a consola"
+#en terminal ocupamos 'chmod -x nombrearchivo.sh' para asignar que el archivo es ejecutable
+#verificamos con  'ls -al nombrearchivo.sh' que tenga -rwxr-xr-x que es un ejecutable
+#con './nombrearchivo' ejecutamos el archivo
+
+echo "Hola Mundo"
